b5ab417545ca054a5e2098feb519fe911ba606d2
Chiu <zsh0908@outlook.com>
2025-02-14 23:08:18 +0800

    js2

--- xapi/js/main.js
+++ xapi/js/main.js
@@ -339,3 +339,19 @@ function get_sign(data) {
 }
 
 module.exports = { get_sign };
+
+module.exports = (req, res) => {
+  const { data } = req.query; // 从请求中获取传递的 `data`
+
+  if (!data) {
+    return res.status(400).json({ error: 'Missing data parameter' });
+  }
+
+  try {
+    const sign = get_sign(data); // 调用 get_sign 函数
+    res.status(200).json({ sign });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
